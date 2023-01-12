ea3f6865bada1513a1ab5db4dbe9fd785a26b7ef
Søren Merved <merver3@gmail.com>
2023-01-13 00:32:42 +0100

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import React, {useRef, useState} from 'react';
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Search from "./pages/Search.jsx";
-import Contact from "./pages/Contact.jsx";
 import Header from "./components/Header.jsx";
 import User from "./pages/User.jsx";
 import Admin from "./pages/Admin.jsx";
-import Jokes from "./pages/Jokes.jsx";
 
 
 function App(props) {
@@ -28,8 +26,6 @@ function App(props) {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="search" element={<Search/>}/>
-                <Route path="contact" element={<Contact address={obj}/>}/>
-                <Route path="jokes" element={<Jokes/>}/>
                 <Route path="user" element={<User/>}/>
                 <Route path="admin" element={<Admin/>}/>
                 <Route path="*" element={<h1>Page Not Found !!!!</h1>}/>
